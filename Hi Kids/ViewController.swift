2cfc260d4c8a9217afaf13fb6e8f54ddabd16c6a
Mike Streff <Mike@Mikes-MacBook-Pro.local>
2016-01-30 17:57:59 -0600

    second upload to github

--- Hi Kids/ViewController.swift
+++ Hi Kids/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hijoe: UIImageView!
+    @IBOutlet weak var joe: UIImageView!
+    @IBOutlet weak var hijoebutton: UIButton!
+    
+    @IBOutlet weak var hiemily: UIImageView!
+    @IBOutlet weak var emily: UIImageView!
+    @IBOutlet weak var hiemilybutton: UIButton!
+    
+    @IBOutlet weak var hiadam: UIImageView!
+    @IBOutlet weak var adam: UIImageView!
+    @IBOutlet weak var hiadambutton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sayhijoe(sender: AnyObject) {
+        hijoe.hidden = false
+        joe.hidden = false
+        hijoebutton.hidden = true
+    }
 
+    @IBAction func sayhiemily(sender: AnyObject) {
+        hiemily.hidden = false
+        emily.hidden = false
+        hiemilybutton.hidden = true
+        
+    }
+    @IBAction func sayhiadam(sender: AnyObject) {
+        hiadam.hidden = false
+        adam.hidden = false
+        hiadambutton.hidden = true
+    }
 }
 
